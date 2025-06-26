ff056a759d83413d24d47c9b260a6198279b330e
gabriel <gabriel.otaciliodev@gmail.com>
2025-06-26 16:01:46 -0300

    criando model de probelma, testado a persistencia

--- test.py
+++ test.py
@@ -1,16 +1,29 @@
 # arquivo pra testar os models.
 
 
-from models.funcionario import Funcionario, FuncionarioModel
+from models.funcionario import  FuncionarioModel,Funcionario
 from models.cliente import Cliente,ClienteModel
-""" --> func_model = FuncionarioModel()
-f = Funcionario(id=111111, name='gabriel', email='gabriel@email.com', birthdate='10-09-2025', salario=2000, lista_pedidos=[10, 20])
-func_model.add(f)
-print(func_model.get_all()) """
+from models.problema import Problema,ProblemaModel
+
+func_model = FuncionarioModel()
+cliente_model=ClienteModel()
+prob_model=ProblemaModel()
+
+p1=Problema(10,'vira-brequin',1200)
+
+prob_model.add(p1)
+print(prob_model.get_all())
+prob_model.delete(1)
+print(func_model.get_all())
+
+""" f = Funcionario(id=10, name='teste', email='gabriel@email.com', birthdate='10-09-2025', salario=2000, lista_pedidos=[10, 20])
+func_model.add(f)  
+print(func_model.get_all())"""
+ 
 
 # como apagar um funcionario cadastrado e ja salvo em json ?
 
-cliente_model=ClienteModel()
-c1 = Cliente(id=24,name='gabriel',email='gabriel@gmail.com',birthdate='15072005')
-cliente_model.add(c1)
-print(cliente_model.get_all())
+
+
+
+
